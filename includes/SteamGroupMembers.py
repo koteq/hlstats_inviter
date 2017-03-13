c4a0a83fe4a028b1d571058aed755be5b4714531
koteq <rereflex@gmail.com>
2017-03-13 22:07:11 +0300

    fix logging

--- includes/SteamGroupMembers.py
+++ includes/SteamGroupMembers.py
@@ -27,18 +27,19 @@ class SteamGroupMembers(object):
         if self._members is None:
             self._members = []
             url = 'http://steamcommunity.com/gid/%s/memberslistxml/?xml=1' % self._group_id
+            logger.debug('Loading steam group members %s', url)
             while True:
-                logger.debug('Requesting %s', url)
                 responce = urllib2.urlopen(url)
                 xml = ElementTree.parse(responce).getroot()
                 members_elements = xml.findall('members/steamID64')
-                logger.info('Found %d members in group %d', len(members_elements), self._group_id)
+
                 members = map(lambda e: e.text, members_elements)
                 self._members.extend(members)
                 next_page = xml.find('nextPageLink')
                 if next_page is not None:
-                    logger.debug('Found next page link')
                     url = next_page.text
+                    logger.debug('Loading steam group members (next page) %s', url)
                 else:
                     break
+            logger.debug('Found %d members in group %s', len(self._members), self._group_id)
         return self._members
